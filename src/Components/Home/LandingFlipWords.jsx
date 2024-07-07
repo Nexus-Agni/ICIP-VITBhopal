698fe2ab0b9f7f6f5cbac667bd3b382f64bf6ad4
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-07-07 22:32:21 +0530

    Update LandingFlipWords component

--- src/Components/Home/LandingFlipWords.jsx
+++ src/Components/Home/LandingFlipWords.jsx
@@ -5,12 +5,15 @@ export function FlipWordsDemo() {
   const words = ["Mathematics", "Science", "Linguistics"];
 
   return (
-    <div className="h-[40rem] flex justify-center items-center px-4">
+    <div className="h-[40rem] flex flex-col justify-center items-center px-4">
       <div className="lg:text-6xl text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
         <div className="lg:text-4xl text-2xl">International Conference on</div>
         Interdisciplinary Perspectives in
         <FlipWords words={words} /> <br />
       </div>
+        <div className="lg:text-2xl mt-6 text-lg text-center text-neutral-500 dark:text-neutral-400">
+            25th - 27th November 2024
+        </div>
     </div>
   );
 }
