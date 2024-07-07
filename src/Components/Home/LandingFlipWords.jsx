b809de643ed72424496b297662e90fb35c96aa13
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-07-07 22:33:19 +0530

    main page

--- src/Components/Home/LandingFlipWords.jsx
+++ src/Components/Home/LandingFlipWords.jsx
@@ -10,6 +10,9 @@ export function FlipWordsDemo() {
         <div className="lg:text-4xl text-2xl">International Conference on</div>
         Interdisciplinary Perspectives in
         <FlipWords words={words} /> <br />
+        <div className="lg:text-4xl text-2xl mt-2">
+          Organized by SASL , VIT Bhopal
+        </div>
       </div>
     </div>
   );
