f29bd0d57ecafb0e81cf6a18b5bf2fd1a5273681
Harsh Balwani <balwaniharsh1001@gmail.com>
2024-07-07 22:22:16 +0530

    main text

--- src/Components/Home/LandingFlipWords.jsx
+++ src/Components/Home/LandingFlipWords.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { FlipWords } from "../ui/flip-words";
 
 export function FlipWordsDemo() {
-  const words = ["better", "cute", "beautiful", "modern"];
+  const words = ["Mathematics", "Science", "Linguistics"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
-      <div className="lg:text-8xl text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-        Build
+      <div className="lg:text-6xl text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+        <div className="lg:text-4xl text-2xl">International Conference on</div>
+        Interdisciplinary Perspectives in
         <FlipWords words={words} /> <br />
-        websites with Aceternity UI
       </div>
     </div>
   );
