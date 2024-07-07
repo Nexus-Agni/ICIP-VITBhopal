99afe0ebd0b14db9b297a146c70a431ff978f8c6
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-07-07 20:39:58 +0530

    Update index.html and package.json
    Add Home component to App.jsx
    
    Remove tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Layout from './Layout'
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import About from './Components/About/About'
 import Contact from './Components/Contact/Contact'
+import Home from './Components/Home/Home'
 
 function App() {
   const route = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout/>}>
+        <Route path='' element={<Home/>}/>
         <Route path='about' element={<About/>}/>
         <Route path='contact' element={<Contact/>}/>
       </Route>
