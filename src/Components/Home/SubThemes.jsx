4fb970ef1ae309546b934b5a4b3b5560ccca0d47
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-07-08 19:23:27 +0530

    Add colors module to tailwind.config.ts

---
+++ src/Components/Home/SubThemes.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { StickyScroll } from '../ui/sticky-scroll-reveal'
+
+function SubThemes() {
+    const content = [
+        {
+            title : "Science and Mathematics Education",
+            description : "Innovative teaching methods in science and mathematics. Curriculum development and evaluation. Integration of technology in science and mathematics education. Assessment and evaluation in science and mathematics education"
+        },
+        {
+            title : "Data Science and Computational Methods",
+            description : "Big Data Analytics, Machine Learning and Artifical Intelligence, Statistical Methods in Data Science, Applicatin of Data Science in various domains.",
+
+        },
+        {
+            title : "Computational Biology",
+            description : "Bioinformatics, System Biology, Computational Genomics, Biological Data Analysis."
+        },
+        {
+            title : "Computational Physics",
+            description : "Numerical Methods in Physics, Simulation and Modelling in Physics, Quantum Computing and its applications, Computational Methods in theoritical Physics."
+        },
+        {
+            title : "Computational Chemistry",
+            description : "Molecular Modelling and simulations, Quantum Chemistry, Cheminformatics, Application of Computational Drug Design."
+        }, 
+        {
+            title : "Mathematical Modelling in Natural Sciences",
+            description : "Mathematical Modelling in Physics, Chemistry, Biology and Linguistics, Applications of Differential Equations, Computational Techniques in Natural sciences and languages, Interdisciplinary applications of mathematical modelling."
+        }, 
+        {
+            title : "Interdisciplinary Approaches to Data Science",
+            description : "Data Science in Social Sciences and Humanities, Ethical issues in Data Science, Data Visualization and Interpretation, Case Studies and Applications."
+        },
+        {
+            title : "Environmental Science and sustainability",
+            description : "Climate Change and Environmental Modelling, Sustainable Development, Environmental Data Analysis, Interdisciplinary approaches to environmental science."
+        },
+        {
+            title : "Cognitive Science and Language",
+            description : "Language acquisition and processing, Cognitive Models of Language understanding, Neurolinguistics, Language and thou."
+        }
+    ]
+  return (
+    <div className='p-10'>
+
+    </div>
+  )
+}
+
+export default SubThemes
