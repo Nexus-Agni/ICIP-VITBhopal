fe48165f62bbcf42914faa22b44a0e0c3c2ebd17
Nexus-Agni <agnibhachakraborty11@gmail.com>
2024-07-09 23:50:50 +0530

    Refactor About and Home components

--- src/Components/Home/Leadership.jsx
+++ src/Components/Home/Leadership.jsx
@@ -5,6 +5,7 @@ import vp from '../../assets/vp.png'
 import avp from '../../assets/avp.png'
 import vc from '../../assets/vc.png'
 import registrar from '../../assets/registrar.png' 
+import { Highlight } from '../ui/hero-highlight'
 export function Leadership() {
 
     const LeadershipItems = [
@@ -35,6 +36,9 @@ export function Leadership() {
         }
     ]
   return (
-    <div className="max-w-5xl mx-auto px-8 flex flex-col md:flex-row flex-wrap items-center justify-center"><HoverEffect items={LeadershipItems} /></div>
+    <div className="max-w-5xl mx-auto px-8 flex flex-col md:flex-row flex-wrap items-center justify-center">
+        <Highlight className='text-yellow-300 text-6xl md:text-8xl'>LEADERSHIP</Highlight>
+        <HoverEffect items={LeadershipItems} />
+    </div>
   )
 }
